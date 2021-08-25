7c1db9753da75c0821a3fde3ae8a28d05c1d2daf
francislxw <luo_xiawu@163.com>
2021-08-25 22:37:22 +0800

    Add files via upload

---
+++ ai-neuralnetworksdeeplearning/1.sequence_model/3.sequence_nideks_and_attention_mechanism/1.neural_machine_translation/run_ai.bat
@@ -0,0 +1,2 @@
+docker run --rm -v %CD%:/myapp -w /myapp machine-translation bash run_in_docker.sh
+
---
+++ ai-neuralnetworksdeeplearning/1.sequence_model/3.sequence_nideks_and_attention_mechanism/1.neural_machine_translation/run_in_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo ----deep-learning:machine-translation----
+
+chmod +x neural_machine_translation.py
+/usr/bin/python3.6 neural_machine_translation.py
